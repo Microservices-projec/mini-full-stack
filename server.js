80d8f2e29f5d1a661cf6927ace800346f04ecd1a
thetanish1 <tanishdewase25@gmail.com>
2025-08-20 19:48:16 +0530

    [full-stack-web] All files are configured and done

--- server.js
+++ server.js
@@ -9,51 +9,59 @@ app.use(cors());
 
 // MySQL Connection
 const db = mysql.createConnection({
-    host: "host.docker.internal",
-    user: "root",      // apna mysql username
-    password: "root",      // apna mysql password
-    database: "productdb"
+  host: "host.docker.internal",
+  user: "root", // apna mysql username
+  password: "root", // apna mysql password
+  database: "productdb",
 });
 
-db.connect(err => {
-    if (err) throw err;
-    console.log(" MySQL Connected...");
+db.connect((err) => {
+  if (err) throw err;
+  console.log(" MySQL Connected...");
 });
 
 // ---------------- API ROUTES ----------------
 
 // Create Product (POST)
 app.post("/products", (req, res) => {
-    const { name, price } = req.body;
-    db.query("INSERT INTO products (name, price) VALUES (?, ?)", [name, price], (err, result) => {
-        if (err) throw err;
-        res.send({ message: "Product Added", id: result.insertId });
-    });
+  const { name, price } = req.body;
+  db.query(
+    "INSERT INTO products (name, price) VALUES (?, ?)",
+    [name, price],
+    (err, result) => {
+      if (err) throw err;
+      res.send({ message: "Product Added", id: result.insertId });
+    }
+  );
 });
 
 // Get All Products (GET)
 app.get("/products", (req, res) => {
-    db.query("SELECT * FROM products", (err, rows) => {
-        if (err) throw err;
-        res.send(rows);
-    });
+  db.query("SELECT * FROM products", (err, rows) => {
+    if (err) throw err;
+    res.send(rows);
+  });
 });
 
 // Update Product (PUT)
 app.put("/products/:id", (req, res) => {
-    const { name, price } = req.body;
-    db.query("UPDATE products SET name=?, price=? WHERE id=?", [name, price, req.params.id], (err) => {
-        if (err) throw err;
-        res.send({ message: "Product Updated" });
-    });
+  const { name, price } = req.body;
+  db.query(
+    "UPDATE products SET name=?, price=? WHERE id=?",
+    [name, price, req.params.id],
+    (err) => {
+      if (err) throw err;
+      res.send({ message: "Product Updated" });
+    }
+  );
 });
 
 // Delete Product (DELETE)
 app.delete("/products/:id", (req, res) => {
-    db.query("DELETE FROM products WHERE id=?", [req.params.id], (err) => {
-        if (err) throw err;
-        res.send({ message: "Product Deleted" });
-    });
+  db.query("DELETE FROM products WHERE id=?", [req.params.id], (err) => {
+    if (err) throw err;
+    res.send({ message: "Product Deleted" });
+  });
 });
 
 // Get single product by ID
@@ -86,4 +94,6 @@ app.get("/products", (req, res) => {
 });
 
 // Start Server
-app.listen(3000, () => console.log("🚀 Server running at http://localhost:3000"));
+app.listen(3000, () =>
+  console.log("🚀 Server running at http://localhost:3000")
+);
